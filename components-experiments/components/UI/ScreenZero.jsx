141f3fc914ca8236bdd32ae3f6261b8249649237
Anodiam <anirban@anodiam.com>
2024-12-12 22:16:58 +1100

    chkin

--- components-experiments/components/UI/ScreenZero.jsx
+++ components-experiments/components/UI/ScreenZero.jsx
@@ -1,6 +1,5 @@
 import { StatusBar, View, useColorScheme } from 'react-native'
 import LabelAnodiam from "./LabelAnodiam"
-import LabelTiffany from "./LabelTiffany"
 import { Colors } from '@/constants/Colors'
 import { screenStyles } from './styles'
 
@@ -14,8 +13,9 @@ const ScreenZero = () => {
     return (
         <View style={styles.anodiamScreen}>
             <StatusBar style={colorScheme} backgroundColor={backgroundColor} />
-            <LabelAnodiam/>
-            <LabelTiffany/>
+            <LabelAnodiam textLabel='AB CD ef #$%^' color={'#46fd5a'} backgroundColor={'#edf021'}
+                            fontFamily={'Arial'} fontSize={40} padding={10} margin={10} textAlign={'right'}
+                            justifyContent={'flex-end'} alignItems={'stretch'}/>
         </View>
     )
 }
