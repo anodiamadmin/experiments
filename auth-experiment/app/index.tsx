c5b17607dfc37850cd526829eb04daefcc64c5c9
Debashish <anodiam.dn@gmail.com>
2024-12-17 13:19:35 +0530

    checked in index

--- auth-experiment/app/index.tsx
+++ auth-experiment/app/index.tsx
@@ -2,10 +2,12 @@ import { View } from "react-native";
 //import ScreenZero from "../components/UI/ScreenZero"
 import SignIn from "../auth/SignIn"
 
-export default function Index() {
+const Index = () => {
   return (
     <View>
       <SignIn/>
     </View>
   );
 }
+
+export default Index;
