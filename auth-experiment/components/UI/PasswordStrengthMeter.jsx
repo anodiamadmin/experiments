a9342377cf6c63a3be3b79abd5d032db5d333b96
Sayananodiam <162682330+Sayananodiam@users.noreply.github.com>
2024-12-29 16:58:09 +0530

    Update PasswordStrengthMeter.jsx
    Updated

--- auth-experiment/components/UI/PasswordStrengthMeter.jsx
+++ auth-experiment/components/UI/PasswordStrengthMeter.jsx
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import * as Progress from 'react-native-progress';
@@ -36,82 +34,17 @@ const PasswordStrengthMeter = (props) => {
                 unfilledColor="#e0e0e0" // Light gray for unfilled portion
                 style={styles.progressBar}
             />
-=======
-=======
->>>>>>> Stashed changes
-import { View, StyleSheet, Dimensions } from 'react-native';
-import React from 'react';
-import { Colors } from '@/constants/Colors';
-
-const PasswordStrengthMeter = (props) => {
-    const passwordStrength = props.createPswdStrength || '';
-
-    // If passwordStrength is empty, render nothing
-    if (passwordStrength === '') {
-        return null;
-    }
-
-    // Total screen width for percentage calculations
-    const screenWidth = Dimensions.get('window').width;
-
-    // Constants for styling
-    const barWidth = screenWidth * 0.16; // Each bar is 18% of screen width
-    const barHeight = 6;
-    const barGap = screenWidth * 0.02; // 2.5% of screen width for spacing
-
-    // Function to render the bars based on strength
-    const renderBars = () => {
-        const bars = [];
-
-        // Define the bar colors based on their position
-        const barColors = [Colors.RED, Colors.RED, Colors.AMBER, Colors.AMBER, Colors.GREEN];
-
-        for (let i = 0; i < passwordStrength; i++) {
-            bars.push(
-                <View
-                    key={i}
-                    style={{
-                        backgroundColor: barColors[i],
-                        width: barWidth,
-                        height: barHeight,
-                        marginRight: i < passwordStrength - 1 ? barGap : 0,
-                    }}
-                />
-            );
-        }
-        return bars;
-    };
-
-    return (
-        <View style={styles.container}>
-            {renderBars()}
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         marginTop: 8,
         width: '100%',
     },
     progressBar: {
         borderRadius: 5,
-=======
-        marginTop: 4,
-        flexDirection: 'row',
-        alignItems: 'center',
->>>>>>> Stashed changes
-=======
-        marginTop: 4,
-        flexDirection: 'row',
-        alignItems: 'center',
->>>>>>> Stashed changes
     },
 });
 
