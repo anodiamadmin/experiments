b70faeaf656fb9f8f433eb8e26d8541fae105470
Debashish <anodiam.dn@gmail.com>
2025-02-17 15:50:32 +0530

    checked in

--- Learn-Typescript/react-native-typescript-demo/app/ScreenZero.tsx
+++ Learn-Typescript/react-native-typescript-demo/app/ScreenZero.tsx
@@ -1,3 +1,18 @@
+/*
+Event Type:
+GestureResponderEvent is the type of event object that is passed to event handler callbacks for touch events in React Native (e.g., onPress, onLongPress, onTouchStart, etc.).
+
+Contents:
+It contains useful information about the gesture, including:
+
+Native Event:
+Information coming from the underlying platform’s gesture system (for example, location, timestamp, etc.).
+Touch Coordinates:
+The position of the touch on the screen.
+Other Details:
+Information about whether the touch is active, if multiple touches are being tracked, etc.
+*/
+
 import { View,GestureResponderEvent,StyleSheet } from 'react-native'
 import LabelAnodiam from "../ui/LabelAnodiam"
 import ButtonAnodiam from "../ui/ButtonAnodiam"
--- Learn-Typescript/react-typescript-demo/src/components/MyComponent/TestTS.tsx
+++ Learn-Typescript/react-typescript-demo/src/components/MyComponent/TestTS.tsx
@@ -1,6 +1,6 @@
 
 export const TestTS = (props: { message: string }) => {
   return (
-    <div>{props.message}</div>
+    <div><h1>{props.message}</h1></div>
   )
 }
