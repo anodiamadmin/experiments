cf4d2ef3e2784bc22569b52dea59ae0257ca60fd
Debashish <anodiam.dn@gmail.com>
2025-02-17 10:53:08 +0530

    checked in

--- Learn-Typescript/react-typescript-demo/src/components/Heading.tsx
+++ Learn-Typescript/react-typescript-demo/src/components/Heading.tsx
@@ -1,3 +1,12 @@
+/* 
+1. This defines a TypeScript type alias called HeadingProps.
+    It is used to specify the shape of an object.
+    children: string;
+
+2. The children property is expected to be of type string.
+    In React, children represents the content inside a component
+*/
+
 type HeadingProps={
     children:string
 }
