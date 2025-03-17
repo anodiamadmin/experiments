d38ac0647c1fffb93ed22e106f794c58e5b8320c
Debashish <66547836+DebashishNath21@users.noreply.github.com>
2025-03-17 17:19:51 +0530

    cmt

--- Learn-Typescript/excel-rpt-proj/src/App.js
+++ Learn-Typescript/excel-rpt-proj/src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 //import DashboardTestCases from './TestCaseMetrics/DashboardTestCases'
-//import CustomLineChart from './Charts/CustomLineChart';
-//import TotalTestCasesChart from './Charts/TotalTestCasesChart';
-//import PassedCasesPercentChart from './Charts/PassedCasesPercentChart';
+
 import AllCharts from './Charts/AllCharts';
 
 function App() {
