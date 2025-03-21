8ea6aa5d3522100f52abce01830ddb834e338897
Debashish <66547836+DebashishNath21@users.noreply.github.com>
2025-03-21 10:18:59 +0530

    checked in

--- Learn-Typescript/excel-rpt-proj/src/Menu/AnodiamRouter.js
+++ Learn-Typescript/excel-rpt-proj/src/Menu/AnodiamRouter.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AnodiamTestCasesDashboard from '../Dashboard/AnodiamTestCasesDasboard';
 import AnodiamTestCasesMenu from './AnodiamTestCasesMenu';
+import TotalTestCasesChart from '../Dashboard/TotalTestCasesChart';
 
 export default function AnodiamRouter() {
   return (
@@ -9,6 +10,7 @@ export default function AnodiamRouter() {
         {/* Default Dashboard */}
         <Route path="/" element={<AnodiamTestCasesDashboard />} />
         <Route path="/menu" element={<AnodiamTestCasesMenu />} />
+        <Route path="/total-test-cases" element={<TotalTestCasesChart />} />
       </Routes>
     </Router>
   );
