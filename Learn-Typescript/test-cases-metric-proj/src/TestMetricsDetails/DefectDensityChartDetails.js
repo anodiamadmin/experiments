62d8beaa0ebb935420e79d927f5b5c6fab85b2c6
Debashish <anodiam.dn@gmail.com>
2025-03-25 10:08:44 +0530

    checked in

--- Learn-Typescript/test-cases-metric-proj/src/TestMetricsDetails/DefectDensityChartDetails.js
+++ Learn-Typescript/test-cases-metric-proj/src/TestMetricsDetails/DefectDensityChartDetails.js
@@ -1,5 +1,3 @@
-//const failedTestPercent = Math.round((currentFailedCases / currentTotalTests) * 100);
-
 import React,{ useEffect, useState } from "react";
 import { Box, Paper, Typography } from '@mui/material';
 import {
