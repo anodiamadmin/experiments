2e2b6dd337648014b4ca1f77eed1a13fef8eee41
Debashish <anodiam.dn@gmail.com>
2025-03-27 13:35:59 +0530

    cmt

--- Learn-Typescript/test-cases-metric-proj/src/Utils/extractConfigInfo.js
+++ Learn-Typescript/test-cases-metric-proj/src/Utils/extractConfigInfo.js
@@ -1,8 +1,10 @@
 const configFileName = "anodiam-test-case-config.json";
 
-const fetchConfigVariables = fetch(configFileName)
-  .then(response => response.json())
-  .catch(error => {
+async function fetchConfigVariables() {
+  try {
+    const response = await fetch(configFileName);
+    return await response.json();
+  } catch (error) {
     console.error('Error fetching ' + configFileName + ': ', error);
     return {
       Environment_Folder: null,
@@ -11,11 +13,14 @@ const fetchConfigVariables = fetch(configFileName)
       Trend_Selector_Last_6_Info: null,
       Trend_Selector_Last_12_Info: null
     };
-  });
+  }
+}
 
-export const Environment_Folder = fetchConfigVariables.then(data => data.Environment_Folder);
-export const Default_Trend_Selector = fetchConfigVariables.then(data => data.Default_Trend_Selector);
+// Fetch the config data synchronously before exporting
+const configData = await fetchConfigVariables();
 
-export const Trend_Selector_Last_3_Info = fetchConfigVariables.then(data => data.Trend_Selector_Last_3_Info);
-export const Trend_Selector_Last_6_Info = fetchConfigVariables.then(data => data.Trend_Selector_Last_6_Info);
-export const Trend_Selector_Last_12_Info = fetchConfigVariables.then(data => data.Trend_Selector_Last_12_Info);
+export const Environment_Folder = configData.Environment_Folder;
+export const Default_Trend_Selector = configData.Default_Trend_Selector;
+export const Trend_Selector_Last_3_Info = configData.Trend_Selector_Last_3_Info;
+export const Trend_Selector_Last_6_Info = configData.Trend_Selector_Last_6_Info;
+export const Trend_Selector_Last_12_Info = configData.Trend_Selector_Last_12_Info;
