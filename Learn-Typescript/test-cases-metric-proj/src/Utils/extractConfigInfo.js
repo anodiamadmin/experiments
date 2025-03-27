d3db40e31ab263d01912d2d550105fd9fa273632
Debashish <anodiam.dn@gmail.com>
2025-03-27 13:25:50 +0530

    cmt

--- Learn-Typescript/test-cases-metric-proj/src/Utils/extractConfigInfo.js
+++ Learn-Typescript/test-cases-metric-proj/src/Utils/extractConfigInfo.js
@@ -1,4 +1,4 @@
-const configFileName="anodiam-test-case-config.json"
+const configFileName = "anodiam-test-case-config.json";
 
 const fetchConfigVariables = fetch(configFileName)
   .then(response => response.json())
@@ -6,9 +6,16 @@ const fetchConfigVariables = fetch(configFileName)
     console.error('Error fetching ' + configFileName + ': ', error);
     return {
       Environment_Folder: null,
-      Default_Trend_Selector: null
+      Default_Trend_Selector: null,
+      Trend_Selector_Last_3_Info: null,
+      Trend_Selector_Last_6_Info: null,
+      Trend_Selector_Last_12_Info: null
     };
   });
 
-  export const Environment_Folder  = fetchConfigVariables.then(data => data.Environment_Folder);
-  export const Default_Trend_Selector  = fetchConfigVariables.then(data => data.Default_Trend_Selector);  
+export const Environment_Folder = fetchConfigVariables.then(data => data.Environment_Folder);
+export const Default_Trend_Selector = fetchConfigVariables.then(data => data.Default_Trend_Selector);
+
+export const Trend_Selector_Last_3_Info = fetchConfigVariables.then(data => data.Trend_Selector_Last_3_Info);
+export const Trend_Selector_Last_6_Info = fetchConfigVariables.then(data => data.Trend_Selector_Last_6_Info);
+export const Trend_Selector_Last_12_Info = fetchConfigVariables.then(data => data.Trend_Selector_Last_12_Info);
