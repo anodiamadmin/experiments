1548c301e2e328f45be211814f0f4463967be31e
Sayananodiam <162682330+Sayananodiam@users.noreply.github.com>
2025-04-26 17:38:56 +0530

    cmt

--- RouterExpoTutorial/app/(tabs)/index.jsx
+++ RouterExpoTutorial/app/(tabs)/index.jsx
@@ -1,6 +1,7 @@
-import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground, Pressable } from 'react-native'
 import React from 'react'
 import icedCoffeeImg from '@/assets/images/iced-coffee.png'
+import { Link } from 'expo-router'
 
 const app = () => {
   return (
@@ -8,9 +9,17 @@ const app = () => {
       <ImageBackground
         source={icedCoffeeImg}
         resizeMode={'cover'}
-        style={styles.imageBackgroundStyle}
+        style={styles.image}
       > 
-        <Text style={styles.textStyle}>Coffee Shop</Text>
+        <Text style={styles.title}>Coffee Shop</Text>
+        <Link style={[styles.link, {marginBottom: 100}]} href={'/contact'}>Reach Out</Link>
+        <Link style={{marginHorizontal:'auto'}} href={'/discover'} asChild>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>
+              Discover Button
+            </Text>
+          </Pressable>
+        </Link>
       </ImageBackground>
     </View>
   )
@@ -21,20 +30,48 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
   },
-  imageBackgroundStyle:{
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
+  image: {
+    width: '100%', 
+    height: 'I00%', 
+    flex: 1, 
+    resizeMode: 'cover', 
     justifyContent: 'center',
   },
-  textStyle: {
-    color: 'violet',
-    fontSize: 42,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)'
-  }
+  title: {
+    color: 'white',
+    fontSize: 42, 
+    fontWeight: 'bold', 
+    textAlign: 'center', 
+    backgroundCoIor: '',
+    marginBottom: 120,
+  }, 
+  link: {
+    color: 'white', 
+    fontSize: 42, 
+    fontWeight: 'bold', 
+    textAlign: 'center', 
+    textDecorationLine: 'underline',
+    backgroundCoIor: '',
+    padding: 4,
+  },
+  button: {
+    height: 60, 
+    borderRadius: 20, 
+    justifyContent: 'center', 
+    backgroundColor: 'rgba(0,0,0,0.75)',
+    padding: 6,
+  },
+  buttonText: {
+    color: 'white', 
+    fontSize: 16, 
+    fontWeight: 'bold', 
+    textAIign: 'center', 
+    padding: 4,
+  }, 
 })
 
 export default app
+
+
+
+ 
