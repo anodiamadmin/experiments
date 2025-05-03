79e63dbb3e5f8b50880bfc457e360acc863f6780
Anodiam <anirban@anodiam.com>
2025-05-03 20:38:34 +1000

    cmt

--- DrawerNavApp/app/index.tsx
+++ DrawerNavApp/app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function ModalIndex() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Index page for Drawer Nav!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 15,
+  },
+});
