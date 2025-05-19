7a41cd1a76b01f26a49972cdbd2e179d1f1d9962
Anodiam <anirban@anodiam.com>
2025-05-19 19:01:38 +1000

    cmt

---
+++ Ubiquitous/Unity/FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FirstConsoleApp
+{
+    /// <summary>
+    /// First console application in .NET 8.0.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// First console application in .NET 8.0.
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // This is a comment in C#.
+            Console.WriteLine("Hello, Anodiam!");
+        }
+    }
+}
